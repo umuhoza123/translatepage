54f0c0d2641be5b2ddc578f5cac1a04d66fca68c
umuhoza123 <84968415+umuhoza123@users.noreply.github.com>
2022-01-23 15:02:48 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,10 @@
 <?php
-$host= "127.0.0.1";
-$pass= "";
-$dbname= "translation";
-$user = "root";
-$connect = new mysqli($host,$user,$pass,$dbname);
-
-if($connect->connect_error)
-{
-    echo "couldn't connect".$connect->connect_error;
-
-}
-else{
-    echo "connected successfull";
-}
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+$connect  = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
